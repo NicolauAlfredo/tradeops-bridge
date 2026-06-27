8cd424262a816022c425b9a85b0077ac72520994
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-06-27 17:51:30 +0200

    feat: add legal content data structure

---
+++ src/data/cookiePolicy.ts
@@ -0,0 +1,7 @@
+import type { LegalDocument } from "../types/legal.type";
+
+export const cookiePolicy: LegalDocument = {
+  lastUpdated: "27 June 2026",
+
+  sections: [],
+};
---
+++ src/data/privacyPolicy.ts
@@ -0,0 +1,7 @@
+import type { LegalDocument } from "../types/legal.type";
+
+export const privacyPolicy: LegalDocument = {
+  lastUpdated: "27 June 2026",
+
+  sections: [],
+};
